d1cc99525edf9f789c9b3c8cc1bb81494a9fafdd
kolapo <garbakolameen@gmail.com>
2023-01-28 21:39:48 +0100

    Exercise 3.4

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
-  { 
+let persons = [
+  {
     "id": 1,
     "name": "Arto Hellas", 
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
     "name": "Ada Lovelace", 
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
     "name": "Dan Abramov", 
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
+  },
+  {
+    "id": 5,
+    "name": "Ameen Garba", 
+    "number": "2348161665061"
+  },
+  {
+    "id": 6,
+    "name": "Jawharat", 
+    "number": "2348146467743"
+  },
+  {
+    "id": 7,
+    "name": "Hamzat",
+    "number": "2348166528841"
   }
 ]
 
@@ -53,7 +68,11 @@ app.get('/api/persons/:id', (req, res) => {
 /* CREATE A RESORCE */
 
 /* DELETE A RESORCE */
-
+app.delete('/api/persons/:id', (req, res) => { 
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
 
 const PORT = 3001;
 app.listen(PORT, function () {
